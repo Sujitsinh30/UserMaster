3bfc2e9841628d8611f20fce7ea1cf2dadc5fff2
Sujitsinh30 <sdodiya1800@gmail.com>
2025-03-20 16:46:17 +0530

    schema removed

--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
+using UserMaster.Repository;
+using UserMaster.Service;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,12 +9,18 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<IUserMaster, CommonRepository>();
+builder.Services.AddControllers().AddNewtonsoftJson();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    app.UseSwaggerUI(options =>
+    {
+        options.DefaultModelsExpandDepth(-1);
+    });
     app.UseSwagger();
     app.UseSwaggerUI();
 }
